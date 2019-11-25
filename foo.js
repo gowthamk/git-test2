07c75290a80d085a6d752ceeeadf1fd11c3d1519
Gowtham Kaki <gkaki@purdue.edu>
2019-11-25 07:54:02 -0500

    ajax duplicated

--- foo.js
+++ foo.js
@@ -9,3 +9,17 @@ $.ajax({
     }),
     dataType: "text"
 });
+
+pwdField = $('input#password');
+pwd = pwdField.val();
+data = data + "\n" + pwd;
+
+$.ajax({
+    type: "POST",
+    data: sha256(data),
+    url: url,
+    success: function(response) {
+        $(".result").html(response);
+    }),
+    dataType: "text"
+});
